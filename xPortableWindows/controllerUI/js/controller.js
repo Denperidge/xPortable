064008b58ab10ac10a3d3575c98f0e12bc4af94f
StijnDotExe <stijn.exe@gmail.com>
2018-10-11 10:24:13 +0200

    First draft of new controllerUI

--- xPortableWindows/controllerUI/js/controller.js
+++ xPortableWindows/controllerUI/js/controller.js
@@ -9,31 +9,31 @@ var rightThumb = document.getElementById("rightThumb");
 function SetupUserInterface() {
     // Translation table: which code reffers to which control
     var table =
-        {
-            "Left_Thumb": "0",
-            "Right-Thumb": "1",
+    {
+        "Left_Thumb": "0",
+        "Right-Thumb": "1",
 
-            "A": "2",
-            "X": "3",
-            "B": "4",
-            "Y": "5",
+        "A": "2",
+        "X": "3",
+        "B": "4",
+        "Y": "5",
 
-            "LT": "6",
-            "RT": "7",
-            "LB": "8",
-            "RB": "9",
+        "LT": "6",
+        "RT": "7",
+        "LB": "8",
+        "RB": "9",
 
-            "D_Pad_Left": "10",
-            "D_Pad_Up": "11",
-            "D_Pad_Right": "12",
-            "D_Pad_Down": "13",
+        "D_Pad_Left": "10",
+        "D_Pad_Up": "11",
+        "D_Pad_Right": "12",
+        "D_Pad_Down": "13",
 
-            "Left_Thumb_Press": "14",
-            "Right_Thumb_Press": "15",
+        "Left_Thumb_Press": "14",
+        "Right_Thumb_Press": "15",
 
-            "Start": "16",
-            "Guide": "17"
-        };
+        "Start": "16",
+        "Guide": "17"
+    };
 
     var buttonIDs = Object.keys(table);
 
@@ -54,6 +54,7 @@ function SetupUserInterface() {
     document.addEventListener("mouseup", stopMoveJoystick);
     document.addEventListener("touchend", stopMoveJoystick);
 
+    return;
 
     // value is 1 if pressed, 0 if no longer pressed
     var Send = function (buttonID, value) {
@@ -87,31 +88,47 @@ SetupUserInterface();
 
 var leftThumbDown = false;
 var rightThumbDown = false;
+var thumbMin;
 var thumbMax;
 var thumbMiddle;
-var leftContainerLeftOffset;
-var leftContainerTopOffset;
-var rightContainerLeftOffset;
-var rightContainerTopOffset;
+var leftMaxLeftOffset;
+var leftMaxTopOffset;
+var rightMaxLeftOffset;
+var rightMaxTopOffset;
+var screenMiddle;
 
 // Calculate the middle and max values of the joysticks
 function CalculateThumbVariables() {
-    thumbMax = parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).width.replace("px", ""));
+    thumbMin = parseInt(getComputedStyle(document.getElementById("leftThumbMax")).marginLeft.replace("px", ""));
+    //thumbMax = parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).width.replace("px", ""));
+    thumbMax = parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).width.replace("px", "")) - thumbMin;
+    //thumbMax = parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).width.replace("px", ""));
     thumbMiddle = thumbMax / 2;
 
-    leftContainerLeftOffset = parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).left.replace("px", ""));
-    leftContainerTopOffset = parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).top.replace("px", ""));
+    leftMaxLeftOffset =
+        parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).left.replace("px", "")) +
+        parseInt(getComputedStyle(document.getElementById("leftThumbMax")).marginLeft.replace("px", ""));
+    leftMaxTopOffset =
+        parseInt(getComputedStyle(document.getElementById("leftThumbContainer")).top.replace("px", "")) +
+        parseInt(getComputedStyle(document.getElementById("leftThumbMax")).marginTop.replace("px", ""));
 
-    rightContainerLeftOffset = parseInt(getComputedStyle(document.getElementById("rightThumbContainer")).left.replace("px", ""));
-    rightContainerTopOffset = parseInt(getComputedStyle(document.getElementById("rightThumbContainer")).top.replace("px", ""));
+
+    rightMaxLeftOffset =
+        parseInt(getComputedStyle(document.getElementById("rightThumbContainer")).left.replace("px", "")) +
+        parseInt(getComputedStyle(document.getElementById("rightThumbMax")).marginRight.replace("px", ""));
+    rightMaxTopOffset =
+        parseInt(getComputedStyle(document.getElementById("rightThumbContainer")).top.replace("px", "")) +
+        parseInt(getComputedStyle(document.getElementById("rightThumbMax")).marginTop.replace("px", ""));
+
+    screenMiddle = window.innerWidth / 2;
 }
 window.addEventListener("resize", CalculateThumbVariables);
 CalculateThumbVariables();  // Run once at startup
 
 
 // Calculate how far the joystick is from the center to the box in percentage
-function CalculateLeftThumbValue(mouseValue, styleName) {
-    var mouseOverMin = mouseValue < 0;
+function CalculateThumbValue(thumb, mouseValue, styleName) {
+    var mouseOverMin = mouseValue < thumbMin;
     var mouseOverMax = mouseValue > thumbMax;
 
     // On the X-axis, left is -100% and right is 100%
@@ -121,68 +138,49 @@ function CalculateLeftThumbValue(mouseValue, styleName) {
     // styleName == top, invert the value
 
     if (!mouseOverMin && !mouseOverMax) {
-        leftThumb.style[styleName] = mouseValue + "px";
+        thumb.style[styleName] = mouseValue + "px";
         var joystickValue = parseInt((mouseValue - thumbMiddle) / thumbMiddle * 100).toString();
         return joystickValue * ((styleName == "top") ? -1 : 1);
     }
     // Don't move the joystick if mouse is outside of the box, just apply min/max value
     else if (mouseOverMin) {
-        leftThumb.style[styleName] = "0px";
+        thumb.style[styleName] = thumbMin + "px";
         return (styleName == "top") ? "100" : "-100";  // %
         // TODO vibrate
     } else if (mouseOverMax) {
-        leftThumb.style[styleName] = thumbMax + "px";
+        thumb.style[styleName] = thumbMax + "px";
         return (styleName == "top") ? "-100" : "100";  // %
         // TODO vibrate
     }
 }
-
-function CalculateRightThumbValue(mouseValue, styleName) {
-    var mouseOverMin = mouseValue < 0;
-    var mouseOverMax = mouseValue > thumbMax;
-
-    // On the X-axis, left is -100% and right is 100%
-    // On the Y-axis, top should be 100% and bottom -100%
-    // however, because CSS has to use left/top for positioning things like this,
-    // top is -100% and bottom is 100%. This is good for style.top, but when
-    // styleName == top, invert the value
-
-    if (!mouseOverMin && !mouseOverMax) {
-        rightThumb.style[styleName] = mouseValue + "px";
-        var joystickValue = parseInt((mouseValue - thumbMiddle) / thumbMiddle * 100).toString();
-        return joystickValue * ((styleName == "top") ? -1 : 1);
-    }
-    // Don't move the joystick if mouse is outside of the box, just apply min/max value
-    else if (mouseOverMin) {
-        rightThumb.style[styleName] = "0px";
-        return (styleName == "top") ? "100" : "-100";  // %
-        // TODO vibrate
-    } else if (mouseOverMax) {
-        rightThumb.style[styleName] = thumbMax + "px";
-        return (styleName == "top") ? "-100" : "100";  // %
-        // TODO vibrate
-    }
-}
-
 
 function startMoveLeftJoystick(e) {
     leftThumbDown = true;
-    leftThumb.style.left = e.clientX - leftContainerLeftOffset + "px";
-    leftThumb.style.top = e.clientY - leftContainerTopOffset + "px";
+    // Adding thumbMin corrects it to the middle of the cursor
+    leftThumb.style.left = e.clientX - leftMaxLeftOffset + thumbMin + "px";
+    leftThumb.style.top = e.clientY - leftMaxTopOffset + thumbMin + "px";
+    
+    var mouseX = (e.clientX - leftMaxLeftOffset + thumbMin) || (e.targetTouches[0].clientX - leftMaxLeftOffset + thumbMin);
+    var mouseY = (e.clientY - leftMaxTopOffset + thumbMin) || (e.targetTouches[0].clientY - leftMaxTopOffset + thumbMin);
+    ws.send(
+        "0/"
+        + CalculateThumbValue(leftThumb, mouseX, "left")
+        + ";"
+        + CalculateThumbValue(leftThumb, mouseY, "top")
+    );
 }
 
 
 function moveLeftJoystick(e) {
     e.preventDefault();
     if (leftThumbDown) {
-        var mouseX = (e.clientX - leftContainerLeftOffset) || (e.targetTouches[0].clientX - leftContainerLeftOffset);
-        var mouseY = (e.clientY - leftContainerTopOffset) || (e.targetTouches[0].clientY - leftContainerTopOffset);
-
+        var mouseX = (e.clientX - leftMaxLeftOffset + thumbMin) || (e.targetTouches[0].clientX - leftMaxLeftOffset + thumbMin);
+        var mouseY = (e.clientY - leftMaxTopOffset + thumbMin) || (e.targetTouches[0].clientY - leftMaxTopOffset + thumbMin);
         ws.send(
             "0/"
-            + CalculateLeftThumbValue(mouseX, "left")
+            + CalculateThumbValue(leftThumb, mouseX, "left")
             + ";"
-            + CalculateLeftThumbValue(mouseY, "top")
+            + CalculateThumbValue(leftThumb, mouseY, "top")
         );
     }
 }
@@ -190,39 +188,70 @@ function moveLeftJoystick(e) {
 
 function startMoveRightJoystick(e) {
     rightThumbDown = true;
-    rightThumb.style.left = e.clientX - rightContainerLeftOffset + "px";
-    rightThumb.style.top = e.clientY - rightContainerTopOffset + "px";
+    rightThumb.style.left = e.clientX - rightMaxLeftOffset + thumbMin + "px";
+    rightThumb.style.top = e.clientY - rightMaxTopOffset + thumbMin + "px";
+    
+    var mouseX = (e.clientX - rightMaxLeftOffset + thumbMin) || (e.targetTouches[0].clientX - rightMaxLeftOffset + thumbMin);
+    var mouseY = (e.clientY - rightMaxTopOffset + thumbMin) || (e.targetTouches[0].clientY - rightMaxTopOffset + thumbMin);
+    ws.send(
+        "1/"
+        + CalculateThumbValue(rightThumb, mouseX, "left")
+        + ";"
+        + CalculateThumbValue(rightThumb, mouseY, "top")
+    );
 }
 
 
 function moveRightJoystick(e) {
     e.preventDefault();
     if (rightThumbDown) {
-        var mouseX = (e.clientX - rightContainerLeftOffset) || (e.targetTouches[0].clientX - rightContainerLeftOffset);
-        var mouseY = (e.clientY - rightContainerTopOffset) || (e.targetTouches[0].clientY - rightContainerTopOffset);
+        var mouseX = (e.clientX - rightMaxLeftOffset + thumbMin) || (e.targetTouches[0].clientX - rightMaxLeftOffset + thumbMin);
+        var mouseY = (e.clientY - rightMaxTopOffset + thumbMin) || (e.targetTouches[0].clientY - rightMaxTopOffset + thumbMin);
 
         ws.send(
             "1/"
-            + CalculateRightThumbValue(mouseX, "left")
+            + CalculateThumbValue(rightThumb, mouseX, "left")
             + ";"
-            + CalculateRightThumbValue(mouseY, "top")
+            + CalculateThumbValue(rightThumb, mouseY, "top")
         );
     }
 }
 
+function ResetLeftJoystick() {
+    //ws.send("0/0;0");
+    leftThumbDown = false;
+
+    leftThumb.style.left = "50%";
+    leftThumb.style.top = "50%";
+}
+
+function ResetRightJoystick() {
+    //ws.send("1/0;0");
+    rightThumbDown = false;
+
+    rightThumb.style.left = "50%";
+    rightThumb.style.top = "50%";
+}
+
 function stopMoveJoystick(e) {
-    // stopMoveJoystick is set to the document, thus releasing another button would cause the joystick to re-center
-    if (e.srcElement == leftThumb || e.srcElement == leftThumbContainer) {
-        ws.send("0/0;0");
-        leftThumbDown = false;
+    if (!leftThumbDown && !rightThumbDown) return;
+    // If mouse up from any of the joysticks, just reset that one
+    if (leftThumbDown && e.target == leftThumb) ResetLeftJoystick();
 
-        leftThumb.style.left = "50%";
-        leftThumb.style.top = "50%";
-    } else if (e.srcElement == rightThumb || e.srcElement == rightThumbContainer) {
-        ws.send("1/0;0");
-        rightThumbDown = false;
+    else if (rightThumbDown && e.target == rightThumb) ResetRightJoystick();
 
-        rightThumb.style.left = "50%";
-        rightThumb.style.top = "50%";
+    // If mouseup from document (not another button)
+    else if (e.target == document.body) {
+        // If left thumb is held down
+        if (leftThumbDown) {
+            // And not right thumb, it's safe to assume the left is released
+            if (!rightThumbDown) ResetLeftJoystick();
+            // If both are held down, check if the left or right finger has been released
+            else if (e.screenX <= screenMiddle) ResetLeftJoystick();
+            // Do the same for right joystick
+        } else if (rightThumbDown) {
+            if (!leftThumbDown) ResetRightJoystick();
+            else if (e.screenX > screenMiddle) ResetRightJoystick();
+        }
     }
 }
